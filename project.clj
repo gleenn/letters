ef65dc8c9b7fd7b4c7e687d1af0f82461ef4f9f1
Glenn Jahnke <gleenn@gmail.com>
2016-12-26 00:28:42 -0800

    wip adding persistence

--- project.clj
+++ project.clj
@@ -1,19 +1,36 @@
 (defproject letters "0.1.0-SNAPSHOT"
-  :dependencies [
-                 [compojure "1.5.1"]
+  :dependencies [[compojure "1.5.1"]
                  [day8.re-frame/http-fx "0.1.3"]
+                 [environ "1.1.0"]
                  [figwheel-sidecar "0.5.0"]
+                 [korma "0.4.2"]
+                 [log4j "1.2.16" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]
+                 [migratus-lein "0.4.3"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
                  [org.clojure/data.json "0.2.6"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
+                 [org.slf4j/slf4j-log4j12 "1.7.9"] ; hidden dep for migratus
                  [ring "1.5.0"]
                  [re-frame "0.9.1"]
-                 [reagent "0.6.0"]
-                 ]
+                 [reagent "0.6.0"]]
 
   :main letters.core
 
-  :plugins [[lein-cljsbuild "1.1.4"]]
+  :plugins [[lein-cljsbuild "1.1.4"]
+            [lein-environ "1.1.0"]
+            [migratus-lein "0.4.2"]]
+
+  :migratus {:store         :database
+             :migration-dir "migrations"
+             :db            {:classname   "com.mysql.jdbc.Driver"
+                             :subprotocol "mysql"
+                             :subname     "//localhost/migratus"
+                             :user        "root"
+                             :password    ""}}
 
   :min-lein-version "2.5.3"
 
@@ -25,6 +42,8 @@
              :css-dirs     ["resources/public/css"]
              :ring-handler letters.server/app
              }
+  ;:ring {:handler letters.server/app}
+
 
   :profiles
   {:dev
---
+++ resources/migrations/20161226000725-create-users.up.sql
@@ -0,0 +1,5 @@
+create table users (
+    id integer,
+ name string,
+      password string,
+       created_at );
