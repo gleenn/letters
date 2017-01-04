59d836bf1ff6e0580968fc0fd4dbe6477860ee90
Glenn Jahnke <gleenn@gmail.com>
2017-01-03 19:12:11 -0800

    working full tree with values

--- src/clj/letters/tree.clj
+++ src/clj/letters/tree.clj
@@ -1,13 +1,13 @@
 (ns letters.tree)
 
+;(defprotocol Node)
+;(defrecord Node [left right value])
+
 (defn complete-tree
-  ([size]
-   (if (> size 1)
-     [(complete-tree (dec size)) (complete-tree (dec size))]
-     [nil nil]))
-  ([size vals]
-   (if (> size 1)
-     (let [[left-vals right-vals] (split-at (/ (count vals) 2) vals)]
-         [(complete-tree (dec size) left-vals)
-        (complete-tree (dec size) right-vals)])
-     (first vals))))
+  [vals]
+  (let [count-vals (count vals)]
+    (if (> count-vals 1)
+      (let [[left-vals right-vals] (split-at (/ count-vals 2) vals)]
+        [(complete-tree (vec left-vals))
+         (complete-tree (vec right-vals))])
+      (first vals))))
--- test/letters/tree_test.clj
+++ test/letters/tree_test.clj
@@ -1,12 +1,16 @@
 (ns letters.tree-test
   (:require [midje.sweet :refer :all]
-            [letters.tree :refer :all]))
+            [letters.tree :refer :all])
+  ;(:import (letters.tree Node))
+  )
 
-(fact tree
-      (complete-tree 1) => [nil nil]
-      (complete-tree 3) => [[[nil nil] [nil nil]] [[nil nil] [nil nil]]]
-      (complete-tree 1 [1]) => 1
-      (complete-tree 2 [1 2]) => [1 2]
-      (complete-tree 3 [1 2 3 4]) => [[1 2] [3 4]]
-      (complete-tree 4 [1 2 3 4 5 6 7 8]) => [[[1 2] [3 4]] [[5 6] [7 8]]]
-      )
+  (fact complete-tree
+      (complete-tree []) => nil
+      (complete-tree [['a 1]]) => ['a 1]
+      (complete-tree [['a 1] ['b 2]]) => [['a 1] ['b 2]]
+      (complete-tree [1 2 3 4]) => [[1 2] [3 4]]
+      (complete-tree [1 2 3 4 5 6 7 8]) => [[[1 2] [3 4]] [[5 6] [7 8]]])
+
+;(fact Node
+;      (Node. 1 2 3) => (Node. 1 2 3)
+;      (Node. 1 nil nil) => (Node. 1 nil nil))
