2db93d2a5da42227c7583520af31e36434e1bef3
Glenn Jahnke <gleenn@gmail.com>
2017-01-05 00:33:58 -0800

    WIP tests passing - started making it more tailored for keeping count; added tree-size func and began pull-letter; started using protocol

--- src/clj/letters/tree.clj
+++ src/clj/letters/tree.clj
@@ -1,13 +1,29 @@
 (ns letters.tree)
 
-;(defprotocol Node)
-;(defrecord Node [left right value])
+(defprotocol NodeType)
+(defrecord Node [left right] NodeType)
+(defn node [left right] (->Node left right))
+(defrecord Leaf [letter count] NodeType)
+(defn leaf [letter count] (->Leaf letter count))
 
-(defn complete-tree
+(defn count-left-tree
   [vals]
   (let [count-vals (count vals)]
     (if (> count-vals 1)
       (let [[left-vals right-vals] (split-at (/ count-vals 2) vals)]
-        [(complete-tree (vec left-vals))
-         (complete-tree (vec right-vals))])
-      (first vals))))
+        (node (count-left-tree (vec left-vals))
+              (count-left-tree (vec right-vals))))
+      (if-let [value (first vals)]
+        (apply leaf value)))))
+
+(defn tree-size [tree]
+  (cond
+    (nil? tree) 0
+    (not (vector? tree)) 1
+    :else (apply + (map tree-size tree))))
+
+(defn pull-letter
+  ([tree]
+   (pull-letter (int (rand (tree-size tree)))))
+  ([tree letter-index]
+    ))
--- test/letters/tree_test.clj
+++ test/letters/tree_test.clj
@@ -1,16 +1,32 @@
 (ns letters.tree-test
   (:require [midje.sweet :refer :all]
-            [letters.tree :refer :all])
-  ;(:import (letters.tree Node))
-  )
+            [letters.tree :refer :all]))
 
-  (fact complete-tree
-      (complete-tree []) => nil
-      (complete-tree [['a 1]]) => ['a 1]
-      (complete-tree [['a 1] ['b 2]]) => [['a 1] ['b 2]]
-      (complete-tree [1 2 3 4]) => [[1 2] [3 4]]
-      (complete-tree [1 2 3 4 5 6 7 8]) => [[[1 2] [3 4]] [[5 6] [7 8]]])
+(fact node
+      (node 1 2) => (node 1 2))
 
+(fact count-left-tree
+      (count-left-tree []) => nil
+      (count-left-tree [['a 1]]) => (leaf 'a 1)
+      (count-left-tree [['a 1] ['b 2]]) => (node (leaf 'a 1) (leaf 'b 2))
+      (count-left-tree [['m 1] ['n 2] ['o 3] ['p 4]]) => (node
+                                                           (node
+                                                             (leaf 'm 1) (leaf 'n 2))
+                                                           (node
+                                                             (leaf 'o 3) (leaf 'p 4)))
+      ;(count-left-tree [1 2 3 4 5 6 7 8]) => [[[1 2] [3 4]] [[5 6] [7 8]]]
+      )
+
+
+(fact tree-size
+      (tree-size nil) => 0
+      (tree-size 'a) => 1
+      (tree-size ['a 'b]) => 2
+      (tree-size [['a 'c] 'b]) => 3)
+
+(fact pull-letter
+      (let [example-tree (count-left-tree [['a 1] ['b 2]])]
+        (pull-letter example-tree 1)))
 ;(fact Node
 ;      (Node. 1 2 3) => (Node. 1 2 3)
 ;      (Node. 1 nil nil) => (Node. 1 nil nil))
